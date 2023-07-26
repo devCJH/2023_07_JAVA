7e9ea015f37dcd1a618017034c20156c7d9fa9c8
devCJH <http-@naver.com>
2023-07-26 17:33:25 +0900

    게시물 검색 기능

--- src/am/App.java
+++ src/am/App.java
@@ -53,17 +53,39 @@ public class App {
 
 				System.out.printf("%d번 게시글이 생성되었습니다\n", lastArticleId);
 
-			} else if (cmd.equals("article list")) {
+			} else if (cmd.startsWith("article list")) {
 
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 					continue;
 				}
+				
+				String searchKeyword = cmd.substring("article list".length()).trim();
+				
+				List<Article> forPrintArticles = articles;
+				
+				if (searchKeyword.length() > 0) {
+					
+					System.out.println("검색어 : " + searchKeyword);
+
+					forPrintArticles = new ArrayList<>();
+					
+					for (Article article : articles) {
+						if (article.title.contains(searchKeyword)) {
+							forPrintArticles.add(article);
+						}
+					}
+					
+					if (forPrintArticles.size() == 0) {
+						System.out.println("검색결과가 없습니다");
+						continue;
+					}
+				}
 
 				System.out.println("번호	|	제목	|		작성일		|	조회수	");
 
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				for (int i = forPrintArticles.size() - 1; i >= 0; i--) {
+					Article article = forPrintArticles.get(i);
 					System.out.printf("%d	|	%s	|	%s	|	%d	\n", article.id, article.title, article.regDate,
 							article.hit);
 				}
